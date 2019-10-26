f5998c8679ecdad5883a074067b19956243098fd
noto90 <mohkarnoto@gmail.com>
2019-10-26 18:37:31 +0700

    menambahkan file 3.php

---
+++ 3.php
@@ -0,0 +1,32 @@
+<?php
+
+function persegi($n)
+{
+    if($n%2==0)
+    {
+        echo "Anda Harus memasukan bilangan ganjil";
+    }
+    else
+    {
+        for ($a = 1; $a <= $n; $a++) {
+            
+            for ($b = 1; $b <= $n; $b++) {
+                //echo $a;
+                if ($a % 2 == 0) {
+                    if($b==1 || $b == $n)
+                    {
+                        echo " * ";
+                    }
+                    else {
+                        echo "= ";
+                    }
+                } else {
+                    echo " * ";
+                }
+            }
+            echo "<br>";
+        }
+    }
+    
+}
+persegi(7);
