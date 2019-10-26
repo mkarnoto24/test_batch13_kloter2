5d561ac7b504732a340d9250fe5de256ed92628e
noto90 <mohkarnoto@gmail.com>
2019-10-26 16:33:59 +0700

    menambahkan file 6.php

---
+++ 6.php
@@ -0,0 +1,35 @@
+<?php
+
+$huruf=[['a','e','c','b','d'],['j','h','i'],['l','k'],['v','y','x','z']];
+$jml=count($huruf);
+sort($huruf);
+echo "[";
+for($x=0;$x<=$jml-1;$x++)
+{
+   echo "[";
+    for($y=0;$y<=count($huruf[$x])-1;$y++)
+    {  
+    
+        //echo sort($huruf);
+        sort($huruf[$x]);
+        if($y<count($huruf[$x])-1)
+        {
+            echo $huruf[$x][$y].",";
+        }
+        else
+        {
+            echo $huruf[$x][$y];
+        }
+    }
+   if($x<$jml-1)
+   {
+        echo "],";
+        
+   }
+   else{
+       echo "]";
+   }
+}  
+echo "]";
+
+?>
